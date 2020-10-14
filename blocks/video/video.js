2eb5b5577ce78e2eae5d849df44a1ee5c4b12217
HappyMarvin <67018534+HappyMarvin@users.noreply.github.com>
2020-10-14 20:22:33 +0300

    video desktop finished (#4)

---
+++ blocks/video/video.js
@@ -0,0 +1,13 @@
+let play;
+function onYouTubePlayerAPIReady() {
+  play = new YT.Player('video__frame', {videoId: 'JMJXvsCLu6s',});
+  document.querySelector('.video__play').onclick = function () {
+    play.playVideo();
+    document.querySelector('.video__overlay').classList.add('video__hide')
+  };
+  play.addEventListener("onStateChange", function (state) {
+    if (state.data === 2) {
+      document.querySelector('.video__overlay').classList.remove('video__hide')
+    }
+  });
+}
